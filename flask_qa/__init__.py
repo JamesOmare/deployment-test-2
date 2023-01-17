f3930688cc72b5522dcbce83ec18bc4a119be1e1
JamesOmare <jamesomare922@gmail.com>
2023-01-17 15:52:56 +0300

    changed __init__.py to deploy on render

--- flask_qa/__init__.py
+++ flask_qa/__init__.py
@@ -28,7 +28,7 @@ def create_app(config = Config):
     with app.app_context():
         db.create_all()
 
-    configure_logging(app)
+    # configure_logging(app)
 
     
     # @app.shell_context_processor
@@ -42,22 +42,22 @@ def create_app(config = Config):
     return app
 
 
-def configure_logging(app):
-    # Logging Configuration
-    if app.config['LOG_WITH_GUNICORN']:
-        gunicorn_error_logger = logging.getLogger('gunicorn.error')
-        app.logger.handlers.extend(gunicorn_error_logger.handlers)
-        app.logger.setLevel(logging.DEBUG)
-    else:
-        file_handler = RotatingFileHandler('instance/flask-test-app.log',
-                                        maxBytes=16384,
-                                        backupCount=20)
-        file_formatter = logging.Formatter('%(asctime)s %(levelname)s %(threadName)s-%(thread)d: %(message)s [in %(filename)s:%(lineno)d]')
-        file_handler.setFormatter(file_formatter)
-        file_handler.setLevel(logging.INFO)
-        app.logger.addHandler(file_handler)
+# def configure_logging(app):
+#     # Logging Configuration
+#     if app.config['LOG_WITH_GUNICORN']:
+#         gunicorn_error_logger = logging.getLogger('gunicorn.error')
+#         app.logger.handlers.extend(gunicorn_error_logger.handlers)
+#         app.logger.setLevel(logging.DEBUG)
+#     else:
+#         file_handler = RotatingFileHandler('instance/flask-test-app.log',
+#                                         maxBytes=16384,
+#                                         backupCount=20)
+#         file_formatter = logging.Formatter('%(asctime)s %(levelname)s %(threadName)s-%(thread)d: %(message)s [in %(filename)s:%(lineno)d]')
+#         file_handler.setFormatter(file_formatter)
+#         file_handler.setLevel(logging.INFO)
+#         app.logger.addHandler(file_handler)
 
-    # Remove the default logger configured by Flask
-    app.logger.removeHandler(default_handler)
+#     # Remove the default logger configured by Flask
+#     app.logger.removeHandler(default_handler)
 
-    app.logger.info('Starting the Flask Test App...')
+#     app.logger.info('Starting the Flask Test App...')
